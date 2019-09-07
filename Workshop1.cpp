24b419ec5a7e88fb2259014aaeda1fd00fc0f131
rechitpatni <rechitpatni@gmail.com>
2019-09-07 12:20:46 +0530

    First Change

---
+++ Workshop1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int a=10;
+
+    cout<<"Learning git and github ";
+    cout<<"Basics of git and github";
+    cout<<a;
+
+    //This is our first version..
+    return 0;
+
+}
